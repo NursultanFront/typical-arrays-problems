c054c42e906f1ba7b10a96fa592ea2a8590eac33
NursultanFront <55098732+NursultanFront@users.noreply.github.com>
2021-02-25 15:36:55 +0500

    feat: add functions for max, min, and avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
+exports.min = function min(array) {
+    if (!array || !array.length) {
+        return 0;
+    }
 
-exports.min = function min (array) {
-  return 0;
-}
+    let minm = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < minm) {
+            minm = array[i];
+        }
+    }
+    return minm;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (!array || !array.length) {
+        return 0;
+    }
 
-exports.avg = function avg (array) {
-  return 0;
-}
+    let maxm = 0;
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] > maxm) {
+            maxm = array[i];
+        }
+    }
+    return maxm;
+};
+
+exports.avg = function avg(array) {
+    if (!array || !array.length) {
+        return 0;
+    }
+
+    let sum = 0;
+    for (var i = 0; i < array.length; i++) {
+        sum = sum + array[i];
+    }
+    return sum / array.length;
+};
